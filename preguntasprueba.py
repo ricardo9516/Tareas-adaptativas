fc38b27e82ec872726dec3b196530ab123ad2df2
ricardo9516 <42320206+ricardo9516@users.noreply.github.com>
2019-09-16 13:15:34 -0500

    Creación de funciones
    Se creó la función de preguntas, esto para generar preguntas random y tener una respuesta correcta en una posición aleatoria. Falta poner la respuesta correcta basándonos en el tema

--- preguntasprueba.py
+++ preguntasprueba.py
@@ -5,20 +5,26 @@ from io import open
 temasU1=['union','interseccion','diferencia']
 rangosU1=[(1,2),(2,4),(4,6),(6,8),(8,12)]
 aciertos=0
-nivel=1
+nivel=0
 ruta="plantillaU1.json"
 
 #Crear JSON
 def crear_datos(ruta):
     contenido= open(ruta,"w")
-    dct=[{"A":1},{"P1":1},{"Respuestas":1}]
+    dct=[{"A":1},{"P1":1},{"R1":1}]
     json_str = contenido.write(json.dumps(dct, indent=True))
+
+#Visualizar datos JSON para crear variables
+def ver_datos(ruta,indice,ubicacion):
+    contenido= open(ruta,"r")
+    plantilla = json.load(contenido)
+    return plantilla[indice][ubicacion]
 
 #Creador de conjuntos
 def create(count):
     S=set()
     while len(S) < count:
-        S.add(randint(0,20))
+        S.add(randint(1,20))
     return S
 
 #Función para modificar datos del JSON
@@ -40,14 +46,29 @@ def entradasU1():
     modificar_datos(ruta,0,"B",B)
 
 #def resultado():
-def respuestas():
+def respuestasU1():
     (low,high) = rangosU1[nivel]
     R1=create(randint(low,high))
     R2=create(randint(low,high))
     R3=create(randint(low,high))
     R4=create(randint(low,high))
     respuestas=[R1,R2,R3,R4]
+    A=ver_datos(ruta,0,"A")
+    B=ver_datos(ruta,0,"B")
+    A=set(A)
+    B=set(B)
+    #unión
+    respuesta_corrrecta=A|B
+    respuestas[randint(0,3)]=respuesta_corrrecta
+    for i in range(4):
+        respuestas[i]=list(respuestas[i])
     print(respuestas)
+    print("A= "+ str(A))
+    print("B= "+ str(B))
+    print("Respuesta= "+ str(respuesta_corrrecta))
+    respuestas_JSON=["R1","R2","R3","R4"]
+    for i in range(4):
+        modificar_datos(ruta,2,respuestas_JSON[i],respuestas[i])
 
 
 
@@ -69,7 +90,8 @@ def preguntasU1():
 
 
 crear_datos(ruta)
-respuestas()
+entradasU1()
+respuestasU1()
 
 """
 #Código principal
